e1a68c275bb8daad227b28e103e3b95a863af510
Peter <petergorman11@gmail.com>
2021-05-08 18:01:55 -0400

    Restore defaults to update game script settings

--- Game Scripts/Updater/settings.bat
+++ Game Scripts/Updater/settings.bat
@@ -3,7 +3,7 @@
 : Required settings:
 : The path to where your steam folder that contains the CryoFall or CryoFall Editor games are.
 : Default is "C:\Program Files (x86)\Steam"
-SET steamFolder="E:\Steam Games"
+SET steamFolder="C:\Program Files (x86)\Steam"
 
 : Optional settings:
 : Whether the empty steam file should be deleted or not
@@ -16,4 +16,4 @@ SET shouldBackup=yes
 : Whether the console window should pause. (Only use this if you're debugging or need to see output)
 : yes = will pause, anything else = will not pause.
 : Default is no
-SET shouldPause=yes
+SET shouldPause=no
